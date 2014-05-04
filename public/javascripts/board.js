97a3a6319d1374ea5b75a64bdc21a3d06722d729
Alex Reichert <alexreichert621@gmail.com>
2014-05-04 13:46:31 -0700

    add simple game logic

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -11,6 +11,42 @@
 		
 		set: function(x, y, mark) {
 			this.grid[y][x] = mark;
+		},
+		
+		won: function() {
+			return ( this.checkRowsCols() || this.checkDiag() );
+		},
+		
+		draw: function() {
+			for (var x = 0; x < 3; x++) {
+				for (var y = 0; y < 3; y++) {
+					if (!this.get(x, y)) { return false; }
+				}
+			}
+			return true;
+		},
+		
+		
+		checkRowsCols: function() {
+			return ( this.checkHoriz(this.grid) || this.checkHoriz(this._transpose()) );
+		},
+		
+		checkDiag: function() {
+			if (this.get(0,0) === this.get(1,1) && this.get(1,1) === this.get(2,2) ||
+					this.get(0,2) === this.get(1,1) && this.get(1,1) === this.get(2,0)) {
+						return this.get(1,1);
+					}
+		},
+		
+		checkHoriz: function(grid) {
+			for (var i = 0; i < 3; i++) {
+				var row = grid[i];
+				if (row[0] === row[1] && row[1] === row[2]) { return row[0] }
+			}
+		},
+		
+		_transpose: function() {
+			return _.zip.apply(_, this.grid);
 		}
 		
 	};
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,26 +1,35 @@
 (function(root) {
 	var TTT = root.TTT = ( root.TTT || {} );
 	var Game = TTT.Game = function(player1, player2) {
-		this.p1 = player1;
-		this.p2 = player2;
+		this.board = new TTT.Board();
+		this.players = { "x": new player1("x", this), "o": new player2("o", this) };
 		this.turn = "x";
-		this.run();
+		// this.run();
 	};
 	
 	Game.prototype = {
 		run: function() {
-			this.players = { "x": new this.p1("x", this), "o": new this.p2("o", this) };
-			this.board = new TTT.Board();
-			this.players[this.turn].move();
+			// this.players[this.turn].move();
 		},
 		
 		set: function(x, y, mark) {
-			this.board.set(x, y, mark);
-			var $cell = $(".cell[data-row='"+y+"'][data-col='"+x+"']");
-			$cell.html(mark);
-			this.turn = mark == "x" ? "o" : "x";
-			this.players[this.turn].move();
-		}
+			if (mark === this.turn) {
+				this.board.set(x, y, mark);
+				var $cell = $(".cell[data-row='"+y+"'][data-col='"+x+"']");
+				$cell.html(mark);
+				var won = this.board.won();
+				var draw = this.board.draw();
+				if (won) {
+					$('body').html(won + " wins!");
+				} else if (draw) {
+					$('body').html("draw!");
+				}
+				this.turn = (mark === "x") ? "o" : "x";
+				// this.players[this.turn].move();
+			}
+		},
+		
+		open: function(x, y) { return !this.board.get(x, y); }
 		
 	};
 	
